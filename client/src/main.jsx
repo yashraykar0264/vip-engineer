091582209588ddbeea5a5b786535cb67954d6e5e
Yash <raykaryash45@gmail.com>
2026-05-11 10:44:28 +0530

    fix router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+<HashRouter>
+  <App />
+</HashRouter>
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
