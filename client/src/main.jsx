2ec87148ecd1479fb59776ca1616aa7ce60ad3c3
Yash <raykaryash45@gmail.com>
2026-05-13 14:21:50 +0530

    Added dynamic explore subjects system with admin management and redesigned home page UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
